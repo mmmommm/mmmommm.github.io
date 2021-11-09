dce4cacb0bf5f763dee236a5459048986eb8c7e2
mmmommm <ryota1839@gmail.com>
2021-11-09 11:08:21 +0900

    change main color and add sakura Color

--- src/variables.ts
+++ src/variables.ts
@@ -1,6 +1,7 @@
-export const mainColor = '#3498db';
+export const mainColor = '#fcd575';
 export const whiteColor = '#f5f5f5';
 export const blackColor = '#22292c';
+export const sakuraColor = '#f0908d';
 
 export const maxWidth = 1005;
 
