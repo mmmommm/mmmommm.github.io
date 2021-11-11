325e5da51aca31f4011f3701293a45b014619b12
mmmommm <mmmommm@users.noreply.github.com>
2021-11-11 02:29:54 +0000

    deploy: 4b3ae1469b1a4bff041ec0243d017ee462812630

---
+++ _next/static/-lfX8RKKXQrm4n81EMLqF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/137-9abb0d5af681c92c92f3.js","static/chunks/pages/index-77f0403cf5ad3d07647a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
