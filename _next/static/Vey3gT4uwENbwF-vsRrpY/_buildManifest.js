cbd94110fee91753725b1a80d397c0aea7670c92
mmmommm <mmmommm@users.noreply.github.com>
2021-11-16 02:29:11 +0000

    deploy: 5bb91d199f3b6257a50b3bf92f293d4524d33ccb

--- _next/static/Vey3gT4uwENbwF-vsRrpY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/197-4b7709bef96de5cf8cc7.js","static/chunks/pages/index-d81c555d9a0f2a6713ee.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
